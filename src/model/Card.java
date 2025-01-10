6d4b2e9f6544dba7724059c9a2b88527e7384135
Justin Iverson <justiniver@icloud.com>
2025-01-10 16:35:01 -0500

    setting up model

---
+++ src/model/Card.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Card {
+  Rank rank;
+  Suit suit;
+
+  Card(Rank rank, Suit suit) {
+    this.rank = rank;
+    this.suit = suit;
+  }
+
+}
---
+++ src/model/Position.java
@@ -0,0 +1,9 @@
+package model;
+
+/**
+ * Represents the positions in heads up poker.
+ */
+public enum Position {
+  SMALL_BLIND, // the 'Dealer'
+  BIG_BLIND // the player 'Out of Position'
+}
