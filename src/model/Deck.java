9875b7514b94f8e25b8ae0850c3b03097158ae5e
Justin Iverson <justiniver@icloud.com>
2025-01-16 21:06:32 -0500

    deck class tbc

---
+++ src/model/Deck.java
@@ -0,0 +1,18 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Deck {
+  List<Card> deck = new ArrayList<>();
+
+  Deck() {
+    for (int i = 0; i < Rank.values().length; i++) {
+      for (int j = 0; j < Suit.values().length; j++) {
+
+      }
+    }
+  }
+
+
+}
