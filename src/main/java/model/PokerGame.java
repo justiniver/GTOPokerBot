bebd17a813903d9af2939d97e4b017f98be1487e
Justin Iverson <justiniver@icloud.com>
2025-02-06 16:18:29 -0500

    poker game set up

--- src/main/java/model/PokerGame.java
+++ src/main/java/model/PokerGame.java
@@ -4,8 +4,12 @@ package model;
  * Represents a poker game.
  */
 public class PokerGame {
+  private GameState state;
   private Deck deck;
-  
+
+  public void PublicGame() {
+    state = GameState.PREFLOP;
+  }
 
 
 }
