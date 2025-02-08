280d5e1d92f067c5b0f7247df92f87fd9808026f
Justin Iverson <justiniver@icloud.com>
2025-02-08 13:53:27 -0500

    poker game simulations started

---
+++ src/main/java/simulations/PokerGameSimulation.kt
@@ -0,0 +1,17 @@
+package simulations
+
+import model.*;
+
+class PokerGameSimulation {
+
+    fun runGameSimulation(trials: Int) {
+
+    }
+
+    fun simulateGame() {
+        val deck = Deck();
+
+    }
+
+
+}
