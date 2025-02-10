cbce1e37341af6fe15e24f400db2c47b3f54c4fb
Justin Iverson <justiniver@icloud.com>
2025-02-09 23:14:58 -0500

    figuring out game

--- src/main/java/simulations/PokerGameSimulation.kt
+++ src/main/java/simulations/PokerGameSimulation.kt
@@ -5,11 +5,15 @@ import model.*;
 class PokerGameSimulation {
 
     fun runGameSimulation(trials: Int) {
-
     }
 
     fun simulateGame() {
+        val game = PokerGame();
         val deck = Deck();
+        val p1 = Position.SMALL_BLIND;
+        val p2 = Position.BIG_BLIND;
+
+
 
     }
 
