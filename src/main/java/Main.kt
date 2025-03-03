df3de77d5480af6ace3773898d5e37d3b720cc09
Justin Iverson <justiniver@icloud.com>
2025-03-03 01:24:19 -0500

    new running process

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -1,13 +1,15 @@
 import simulations.PokerShowdownSim
 import visualization.ProbabilityChart
+import util.CardStrings
 
 fun main() {
+    val cs = CardStrings()
     val simulation = PokerShowdownSim()
     val trialsList = (50..10000 step 10).toList()
     val probabilities = mutableListOf<Double>()
 
     for (trials in trialsList) {
-        simulation.runShowdownDeucesSimulation(trials)
+        simulation.runShowdownSimulation(trials, cs.eightSpade, cs.tenSpade)
         val probability = simulation.getWinCount().toDouble() / simulation.getTrialsCount()
         probabilities.add(probability)
     }
