7d5ee7c01f2832724d83928d6572e4d057bd328b
Justin Iverson <justiniver@icloud.com>
2025-03-09 20:23:26 -0400

    starting betting round

--- src/main/java/model/BettingRound.java
+++ src/main/java/model/BettingRound.java
@@ -1,8 +1,14 @@
 package model;
 
 public class BettingRound {
-  Player player1;
-  Player player2;
+  Player smallBlind;
+  Player bigBlind;
+
+  public BettingRound() {
+    this.smallBlind = new Player(Position.SMALL_BLIND);
+    this.bigBlind = new Player(Position.BIG_BLIND);
+
+  }
 
 
 
