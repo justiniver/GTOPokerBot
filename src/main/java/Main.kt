35f4cbcf2499be3cb49c0672f2d6c3d2693f8f25
Justin Iverson <justiniver@icloud.com>
2025-05-02 22:04:30 +0900

    cleaning up main

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -15,20 +15,3 @@ fun main() {
     pokerController.playHand(pokerGame)
 }
 
-// create "how to use" section for README and add the code below as an example on how to run
-// equities
-
-//    val cs = CardStrings()
-//    val simulation = PokerShowdownSim()
-//    val trialsList = (50..10000 step 10).toList()
-//    val probabilities = mutableListOf<Double>()
-//
-//    for (trials in trialsList) {
-//        simulation.runShowdownSimulation(trials, cs.queenDiamond, cs.tenDiamond)
-//        val probability = simulation.getWinCount().toDouble() / simulation.getTrialsCount()
-//        probabilities.add(probability)
-//    }
-//
-//    val chart = ProbabilityChart()
-//    chart.displayChart(trialsList, probabilities, "Queen-Ten Suited Probability Showdown")
-
