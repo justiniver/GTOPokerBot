78d6bffa401bb1edb967d0b17910456287cc083b
Justin Iverson <justiniver@icloud.com>
2025-05-04 10:59:00 +0900

    setting up poker session class

--- src/main/java/model/PokerSession.java
+++ src/main/java/model/PokerSession.java
@@ -1,10 +1,23 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PokerSession {
   private PokerGame pokerGame;
+  private Map<HandRank, Integer> winningHandRankFreq;
 
   public PokerSession(PokerGame pokerGame) {
     this.pokerGame = pokerGame;
+    this.winningHandRankFreq = new HashMap<>();
+  }
+
+  public void startNewGame(PokerGame pokerGame) {
+
+  }
+
+  public Map getWinningHandRankFreq() {
+    return winningHandRankFreq;
   }
 
   public PokerGame getPokerGame() {
