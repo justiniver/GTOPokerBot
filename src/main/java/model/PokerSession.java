481c44960ba03b4dd4fde02bfeeac437254a3bd8
Justin Iverson <justiniver@icloud.com>
2025-05-05 00:40:30 +0900

    setting up poker session;

--- src/main/java/model/PokerSession.java
+++ src/main/java/model/PokerSession.java
@@ -1,14 +1,24 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PokerSession {
   private PokerGame pokerGame;
   private Map<HandRank, Integer> winningHandRankFreq;
+  private final int smallBlindAmount;
+  private final int bigBlindAmount;
+  private final Player playerSB;
+  private final Player playerBB;
 
-  public PokerSession(PokerGame pokerGame) {
-    this.pokerGame = pokerGame;
+  public PokerSession(int smallBlindAmount, int bigBlindAmount,
+                      Player playerSB, Player playerBB) {
+    this.smallBlindAmount = smallBlindAmount;
+    this.bigBlindAmount = bigBlindAmount;
+    this.playerSB = playerSB;
+    this.playerBB = playerBB;
     this.winningHandRankFreq = new HashMap<>();
   }
 
@@ -16,9 +26,10 @@ public class PokerSession {
 
   }
 
-  public Map getWinningHandRankFreq() {
-    return winningHandRankFreq;
+  public List<Integer> getWinningHandRankFreq() {
+    return new ArrayList<>(winningHandRankFreq.values());
   }
+
 
   public PokerGame getPokerGame() {
     return pokerGame;
