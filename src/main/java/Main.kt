4686655670838472044f9be01313e61b482736ce
Justin Iverson <justiniver@icloud.com>
2025-07-13 19:12:39 -0400

    "

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -12,8 +12,6 @@ fun main() {
 
     val playerSB = Player(Position.SMALL_BLIND, 1000)
     val playerBB = Player(Position.BIG_BLIND, 1000)
-    // playerSB.strategy = aggressiveBot
-    // playerBB.strategy = conservativeBot
 
     val session = PokerSession(5, 10, playerSB, playerBB)
 
