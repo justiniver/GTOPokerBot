7b7d514833fc3be7f821909ca086ab076486d260
Justin Iverson <justiniver@icloud.com>
2025-07-19 00:04:23 -0400

    debugging missing chips mystery

--- src/main/java/Main.kt
+++ src/main/java/Main.kt
@@ -5,13 +5,13 @@ import model.Position
 import model.PokerSession
 
 fun main() {
-    // val aggressiveBot = RuleBasedBot(0.5, 0.5)
-    // val conservativeBot = RuleBasedBot(0.5, 0.5)
 
     // ^ results in 'Invalid action. Not enough chips to call' -- FIX
-
     val playerSB = Player(Position.SMALL_BLIND, 1000)
     val playerBB = Player(Position.BIG_BLIND, 1000)
+
+    playerSB.strategy = SimpleCheckCallBot()
+    playerBB.strategy = SimpleCheckCallBot()
 
     val session = PokerSession(5, 10, playerSB, playerBB)
 
