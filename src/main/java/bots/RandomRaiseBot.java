e7426a089b1de600888bead1bdb6342532e108fb
Justin Iverson <justiniver@icloud.com>
2025-08-20 12:18:45 +0900

    simple random bot

--- src/main/java/bots/RandomRaiseBot.java
+++ src/main/java/bots/RandomRaiseBot.java
@@ -6,21 +6,24 @@ public class RandomRaiseBot implements PlayerStrategy {
 
   @Override
   public Decision decide(GameView view) {
-    Random rand = new Random();
-    if (rand.nextBoolean()) {
-      int amtCall = view.toCall();
-      if (amtCall == 0) {
+    int amtCall = view.toCall();
+    if (amtCall == 0) {
+      Random rand = new Random();
+      if (rand.nextBoolean()) {
         int amtToBet = (int) (view.pot() * rand.nextDouble(0.30, 1.30));
-        if (amtToBet < view.bigBlindAmount()) {
-          amtToBet = view.bigBlindAmount(); // need to make sure that we can afford this as well.
-                                            // thinking that maybe I just create an all in amt which can always be shoved (regardless of minbet)
-        } else if (amtToBet > view.myStack()) {
+
+        if (amtToBet > view.myStack()) {
           amtToBet = view.myStack();
+        } else if (amtToBet < view.bigBlindAmount() || amtToBet < amtCall) {
+          amtToBet = view.bigBlindAmount();
         }
+
+        return new Decision(Action.BET, amtToBet);
+      } else {
+        return Decision.fold();
       }
     }
 
-    // logic to bet, raise, reraise randomly and safely
-
+    return Decision.fold();
   }
 }
