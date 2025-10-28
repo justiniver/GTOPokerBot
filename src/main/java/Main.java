862e05937e61bba3940b7040a2e2f2b1dcc1b4f4
Justin Iverson <justiniver@icloud.com>
2025-10-28 10:45:05 -0400

    testing different iteration sizes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,12 @@ import model.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Rank.values()[0]);
+        Player human = new Player(Position.SMALL_BLIND, 1000);
+        Player bot = new Player(Position.BIG_BLIND, 1000);
 
-//        Player human = new Player(Position.SMALL_BLIND, 1000);
-//        Player bot = new Player(Position.BIG_BLIND, 1000);
-//
-//        TrainedSession trainedSession =
-//                new TrainedSession(5, 10, human, bot, 1000);
-//
-//        trainedSession.run();
+        TrainedSession trainedSession =
+                new TrainedSession(5, 10, human, bot, 10000);
+
+        trainedSession.run();
     }
 }
